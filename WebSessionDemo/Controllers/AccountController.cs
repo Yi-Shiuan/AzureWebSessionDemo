2338c75600ad6ff6122d1f4ad36d2317ec273d19
yi-shiuan <amour99520@gmail.com>
2017-04-13 22:53:15 +0800

    session

--- WebSessionDemo/Controllers/AccountController.cs
+++ WebSessionDemo/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using System.Text;
 
 namespace WebSessionDemo.Controllers
 {
@@ -17,6 +18,7 @@ namespace WebSessionDemo.Controllers
             new Claim(ClaimTypes.Name, "test"),
             new Claim(ClaimTypes.Sid, "12345")
         };
+            HttpContext.Session.Set("test", Encoding.UTF8.GetBytes("test"));
             var claimsIdentity = new ClaimsIdentity(claims, "password");
             var claimsPrinciple = new ClaimsPrincipal(claimsIdentity);
             await HttpContext.Authentication.SignInAsync("Cookies", claimsPrinciple);
